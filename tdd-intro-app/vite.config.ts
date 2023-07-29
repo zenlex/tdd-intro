e28957dbe5926e47ab7a81a216b171c35911e1a3
zenlex <erichrkeil@gmail.com>
2023-07-29 16:17:51 -0500

    vite config and folder cleanup and install happy-dom

--- tdd-intro-app/vite.config.ts
+++ tdd-intro-app/vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [Vue()],
 
   test: {
-    exclude: ['**/node_modules/**', '**/tests/e2e/**', '**/dist/**']
+    exclude: ['**/node_modules/**', '**/tests/e2e/**', '**/dist/**'],
+    environment: 'happy-dom'
   }
 })
